0d5019bbb3ac55d013b0596f72e8ed514ad9fe8b
Jon Takagi <jontakagi@reed.edu>
2020-05-04 23:35:29 +0000

    improved by 30% ish

--- README.md
+++ README.md
@@ -5,8 +5,12 @@ The default implementation got a best run time of 0.03467s on my VM.
 
 ### Naive
 
-My naive implementation got a best run time of .007276 seconds on my VM. It assumes that all quotes are between 3 and 5 digits, since all values in the quotes set are between 100 and 62951.
+My naive implementation got a best run time of .0072s on my VM. It assumes that all quotes are between 3 and 5 digits, since all values in the quotes set are between 100 and 62951.
 
 The ASCII code for 0 is 48, so interpreting a char as an ASCII value and subtracting 48 gives the int value. My code then checks the length by checking the 6th, 5th and 4th characters - if they are the null terminator, then the line is 5, 4 or 3 digits respectively.
 
 ### Optimization
+
+Removing the "digits" array, and simply using the contents - `(lines[i][0]-48)` lets the compiler skip moving a bunch of values around. This gets me down to .0046s, which is 63% of the original time.
+
+Examining the math calculation, the multiplication can be expanded, but this makes it worse by .0005s or so.
--- converter.c
+++ converter.c
@@ -5,15 +5,15 @@
 #include "converter.h"
 #include <iostream>
 #include <stdlib.h>
-
+#include <emmintrin.h>
 // EXAMPLE
 
-void convert_all(unsigned nlines, char *lines[], quote_t nums[])
-{
-  for (unsigned i = 0; i < nlines; i++) {
-    nums[i] = atoi(lines[i]);
-  }
-}
+// void convert_all(unsigned nlines, char *lines[], quote_t nums[])
+// {
+//   for (unsigned i = 0; i < nlines; i++) {
+//     nums[i] = atoi(lines[i]);
+//   }
+// }
 
 // NAIVE
 
@@ -45,3 +45,26 @@ void convert_all(unsigned nlines, char *lines[], quote_t nums[])
         }
     }
 }
+
+// Improved:
+
+// void convert_all(unsigned nlines, char *lines[], quote_t nums[])
+// {
+//     int length = 5;
+//     for (unsigned i = 0; i < nlines; i++) {
+//         if(lines[i][3] == '\0') {
+//             length = 3;
+//         } else if(lines[i][4] == '\0') {
+//             length = 4;
+//         } else {
+//             length = 5;
+//         }
+//         if(length == 3) {
+//             nums[i] = 100 * (lines[i][0] - 48) + 10 * (lines[i][1] - 48) + (lines[i][2] - 48);
+//         } else if(length == 4) {
+//             nums[i] = 1000 * (lines[i][0] - 48) + 100 * (lines[i][1] - 48) + 10 * (lines[i][2] - 48) + (lines[i][3] - 48);
+//         } else {
+//             nums[i] = 10000 * (lines[i][0] - 48) + 1000 * (lines[i][1] - 48) + 100 * (lines[i][2] - 48) + 10 * (lines[i][3] - 48) + (lines[i][4] - 48);
+//         }
+//     }
+// }
